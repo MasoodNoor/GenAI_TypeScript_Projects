689661691e7fe34f24816fa100e079946aaa3dc4
MasoodNoor <themasoodnoor100@gmail.com>
2024-04-04 01:25:42 +0500

    adding receipt

--- project03_atm_machine/main.js
+++ project03_atm_machine/main.js
@@ -1,67 +1,79 @@
 import chalk from "chalk";
 import inquirer from "inquirer";
 console.clear();
-console.log(chalk.white.bgBlue.bold('\t\tWELCOME TO GENAI ATM MACHINE\t\t'));
+console.log(chalk.white.bgBlue.bold("\t\tWELCOME TO GENAI ATM MACHINE\t\t"));
 console.log("Please register for new account");
 var userDetalis = await inquirer.prompt([
     {
         name: "userName",
         type: "String",
-        message: "Please enter your name"
+        message: "Please enter your name",
     },
     {
         name: "userPin",
         type: "number",
-        message: "Please enter your pin"
+        message: "Please enter your pin",
     },
 ]);
 console.log("Thank you signing up, Please enter your details for withdrawal");
 console.log(`Your name ${userDetalis.userName} and your password ${userDetalis.userPin}`);
-var userChoice = await inquirer.prompt([{
+var userChoice = await inquirer.prompt([
+    {
         name: "action",
         type: "list",
         message: "Do you want to continue?",
         choices: ["Login", "Cancel"],
-    }]);
+    },
+]);
 if (userChoice.action == "Login") {
     console.clear();
     let userId = await inquirer.prompt([
         {
             name: "userNameId",
             type: "String",
-            message: "Please enter your name"
+            message: "Please enter your name",
         },
         {
             name: "userPinId",
             type: "number",
-            message: "Please enter your pin"
-        }
+            message: "Please enter your pin",
+        },
     ]);
     var userBalance = 30000;
-    if (userDetalis.userName == userId.userNameId && userDetalis.userPin == userId.userPinId) {
+    if (userDetalis.userName == userId.userNameId &&
+        userDetalis.userPin == userId.userPinId) {
         console.log("You have logged in successfully");
         let accountDeatils = await inquirer.prompt([
             {
                 name: "accountType",
                 type: "list",
                 message: "Please select your account type",
-                choices: ["Savings", "Current"]
-            }
+                choices: ["Savings", "Current"],
+            },
         ]);
         while (true) {
-            let userAccount = await inquirer.prompt([{
+            let userAccount = await inquirer.prompt([
+                {
                     name: "action",
                     type: "list",
                     message: "What you want to do today?",
-                    choices: ["Withdrawal", "Quick Withdrawal", "Balance Inquiry", "Cancel"],
-                }]);
+                    choices: [
+                        "Withdrawal",
+                        "Quick Withdrawal",
+                        "Balance Inquiry",
+                        "Cancel",
+                    ],
+                },
+            ]);
             if (userAccount.action == "Withdrawal") {
                 console.clear();
-                let withdrawal = await inquirer.prompt([{
+                let withdrawal = await inquirer.prompt([
+                    {
                         name: "amount",
                         type: "number",
                         message: "Please enter the amount you want to withdraw",
-                    }]);
+                    },
+                ]);
                 if (withdrawal.amount > userBalance) {
                     console.log("You have insufficient balance");
                 }
@@ -78,18 +90,20 @@ if (userChoice.action == "Login") {
             }
             else if (userAccount.action == "Quick Withdrawal") {
                 console.clear();
-                let withdrawal = await inquirer.prompt([{
-                        name: "amount",
+                let quickWithdrawal = await inquirer.prompt([
+                    {
+                        name: "quickamount",
                         type: "list",
                         message: "Please enter the amount you want to withdraw",
-                        choices: ["5000", "10000", "15000", "20000", "25000"]
-                    }]);
-                if (withdrawal.amount > userBalance) {
+                        choices: [5000, 10000, 15000, 20000, 25000],
+                    },
+                ]);
+                if (quickWithdrawal.quickamount > userBalance) {
                     console.log("You have insufficient balance");
                 }
                 else {
-                    console.log("You have withdrawn " + withdrawal.amount);
-                    userBalance -= withdrawal.amount;
+                    console.log("You have withdrawn " + quickWithdrawal.quickamount);
+                    userBalance -= quickWithdrawal.quickamount;
                     console.log("Your new balance is " + userBalance);
                     break;
                 }
@@ -102,10 +116,28 @@ if (userChoice.action == "Login") {
                 console.log("Thank you, See you soon");
                 break;
             }
-            else {
-                console.log("You have entered wrong credentials");
-            }
         }
+        let userReceipt = await inquirer.prompt([
+            {
+                name: "receipt",
+                type: "list",
+                message: `${chalk.bold.bgYellow("Do you want to print your receipt?")}`,
+                choices: ["Yes", "No"],
+            }
+        ]);
+        if (userReceipt.receipt == "Yes") {
+            console.clear;
+            console.log(`${chalk.bold.bgWhite("--------------------------------")}`);
+            console.log();
+            console.log();
+            console.log(`${chalk.bold.bgWhite("--------------------------------")}`);
+        }
+        else if (userReceipt.receipt == "No") {
+            console.log("Thank you, See you soon");
+        }
+    }
+    else {
+        console.log("You have entered wrong credentials");
     }
 }
 else if (userChoice.action == "Cancel") {
